bb9fbbd51b3500a823af901285b3c99d37570dc2
Phat Ma <phat.ma@hotmail.co.uk>
2018-09-10 15:44:20 +0100

    adds semantic input field

--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import { Input } from 'semantic-ui-react';
 
 class SearchBox extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    this.setState({
+      searchTerm: e.target.value
+    });
+  };
   render() {
     return (
-      <div>
-        <Input icon="search" placeholder="Search..." />
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <Input
+          action={{ icon: 'search' }}
+          placeholder="Search..."
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
+        />
+      </form>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Css
-import './style/style.css';
 import '../semantic/dist/semantic.min.css';
 // React components
 import App from './components/app';
