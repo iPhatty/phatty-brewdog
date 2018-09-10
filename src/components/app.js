a0fec0cd39963b9bc31ebe51049eb8ea61480507
Phat Ma <phat.ma@hotmail.co.uk>
2018-09-10 19:04:02 +0100

    creates data fetch

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,37 @@ import { hot } from 'react-hot-loader';
 
 // Components
 import SearchBox from './searchBox';
+
 class App extends Component {
+  state = {
+    data: null
+  };
+
+  getData = searchTerm => {
+    const url = `https://api.punkapi.com/v2/beers?food=${searchTerm}`;
+    fetch(url)
+      .then(response => {
+        if (response.status !== 200) {
+          console.log(
+            'Looks like there was a problem. Status Code: ' + response.status
+          );
+          return;
+        }
+        // Examine the text in the response
+        response.json().then(data => {
+          this.setState({ data }, () => console.log(this.state));
+        });
+      })
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+  };
+
   render() {
     return (
       <div>
         iPhatty React
-        <SearchBox />
+        <SearchBox getData={this.getData} />
       </div>
     );
   }
