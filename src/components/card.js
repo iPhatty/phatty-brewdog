4d24de3e2b69b3f108d4e8c77dbf3fa355e08c88
Phat Ma <phat.ma@hotmail.co.uk>
2018-09-25 13:46:35 +0100

    applys simple transition onMount

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class BeerCard extends Component {
+  state = {
+    didMount: false
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ didMount: true });
+    }, 0);
+  }
+
   foodPairing = () => {
     return this.props.food_pairing.map(pairing => {
       return <li key={pairing}>{pairing}</li>;
@@ -10,7 +20,10 @@ class BeerCard extends Component {
   render() {
     const { image_url, name, tagline, description } = this.props;
     return (
-      <li className="list--item">
+      <li
+        className={`list--item fade-in-bottom ${this.state.didMount &&
+          'active'}`}
+      >
         <div className="card">
           <img className="card--img" src={image_url} />
           <h2>{name}</h2>
