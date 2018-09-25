6eabfc7e2ce45121cf1629383dfa06b0d1f9d23a
Phat Ma <phat.ma@hotmail.co.uk>
2018-09-25 13:34:59 +0100

    changes card component to class based

--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BeerCard = props => {
-  const foodPairing = props.food_pairing.map(pairing => {
-    return <li key={pairing}>{pairing}</li>;
-  });
+class BeerCard extends Component {
+  foodPairing = () => {
+    return this.props.food_pairing.map(pairing => {
+      return <li key={pairing}>{pairing}</li>;
+    });
+  };
 
-  return (
-    <li className="list--item">
-      <div className="card">
-        <img className="card--img" src={props.image_url} />
-        <h2>{props.name}</h2>
-        <p>
-          <span className="date">{props.tagline}</span>
-        </p>
-        <p>{props.description}</p>
-        <p>Goes well with:</p>
-        <ul>{foodPairing}</ul>
-      </div>
-    </li>
-  );
-};
+  render() {
+    const { image_url, name, tagline, description } = this.props;
+    return (
+      <li className="list--item">
+        <div className="card">
+          <img className="card--img" src={image_url} />
+          <h2>{name}</h2>
+          <p>
+            <span className="date">{tagline}</span>
+          </p>
+          <p>{description}</p>
+          <p>Goes well with:</p>
+          <ul>{this.foodPairing()}</ul>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default BeerCard;
